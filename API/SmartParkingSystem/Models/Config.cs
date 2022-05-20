fb748f84c5c57556963b7101c67c9c3758262dd0
enesfurkancali <67002809+enesfurkancali@users.noreply.github.com>
2022-05-20 14:41:58 +0300

    .

---
+++ API/SmartParkingSystem/Models/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SmartParkingSystem.Models
+{
+    public partial class Config
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ API/SmartParkingSystem/Models/Entry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace SmartParkingSystem.Models
+{
+    public partial class Entry
+    {
+        public Guid Id { get; set; }
+        public string Plate { get; set; }
+        public double? Price { get; set; }
+        public DateTime? CheckoutDate { get; set; }
+        public DateTime? CheckinDate { get; set; }
+    }
+}
