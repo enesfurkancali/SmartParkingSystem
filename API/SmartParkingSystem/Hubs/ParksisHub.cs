1da8aa89f7ac5bb03cd456837086f5ec767f9543
enesfurkancali <67002809+enesfurkancali@users.noreply.github.com>
2022-06-28 20:28:26 +0300

    Update

--- API/SmartParkingSystem/Hubs/ParksisHub.cs
+++ API/SmartParkingSystem/Hubs/ParksisHub.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SmartParkingSystem.Models;
+using SmartParkingSystemAPI.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,34 @@ namespace SmartParkingSystemAPI.Hubs
 {
     public class ParksisHub:Hub
     {
+        private readonly SmartParkingSystemContext _context;
+
+        public ParksisHub(SmartParkingSystemContext context)
+        {
+            _context = context;
+        }
+
         public async Task SendMessageAsync(string plate)
         {
-            await Clients.All.SendAsync("recievePlate", plate);
+            var entry = _context.Entries.FirstOrDefault(x => x.Plate == plate && x.CheckoutDate == null);
+            if (entry is not null)
+            {
+                entry.CheckoutDate = DateTime.Now;
+                TimeSpan diff = (TimeSpan)(entry.CheckoutDate - entry.CheckinDate);
+                CalculatePrice.price = 0;
+                CalculatePrice.priceList = _context.Configs.ToList();
+                entry.Price = CalculatePrice.Calculate(diff);
+            }
+            else
+            {
+                entry = new Entry();
+                entry.Id = Guid.NewGuid();
+                entry.CheckinDate = DateTime.Now;
+                entry.CheckoutDate = null;
+                entry.Plate = plate.Trim();
+                entry.Price = 0;
+            }
+            await Clients.All.SendAsync("recievePlate", entry);
         }
     }
 }
